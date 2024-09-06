5d65b6954d1ed6c3d0d9a1cd35e90efe0e05f5bd
Haydn Ewers <27211+haydn@users.noreply.github.com>
2024-09-06 10:34:56 +1000

    namespaced worknice webhook handlers

--- src/app/worknice/integration-created/route.ts
+++ src/app/worknice/integration-created/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { z } from "zod";
 import { createZodFetcher } from "zod-fetch";
-import redis from "../../redis";
+import redis from "../../../redis";
 
 const fetchWithZod = createZodFetcher();
 
