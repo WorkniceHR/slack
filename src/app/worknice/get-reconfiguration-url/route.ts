3e01bedc8defb381b3c6310f05cd877fade20953
matthewknee <matt@worknice.com>
2024-09-12 09:36:13 +1000

    Added return url

--- src/app/worknice/get-reconfiguration-url/route.ts
+++ src/app/worknice/get-reconfiguration-url/route.ts
@@ -1,1 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
 
+    return NextResponse.json(
+      {
+        reconfigurationUrl: "https://slack.worknice.com/reconfig",
+      },
+      { status: 200 },
+    );
