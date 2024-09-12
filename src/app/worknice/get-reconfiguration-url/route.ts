7b535dc04055b46cdad8a1eeb05795ec1bfe9f9b
matthewknee <matt@worknice.com>
2024-09-12 14:16:22 +1000

    Put the return within a function and error catch

--- src/app/worknice/get-reconfiguration-url/route.ts
+++ src/app/worknice/get-reconfiguration-url/route.ts
@@ -1,8 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const POST = async (request: NextRequest): Promise<NextResponse> => {
+  try {
+
     return NextResponse.json(
       {
-        reconfigurationUrl: "https://slack.worknice.com/reconfig",
+        authorizationUrl: "https://slack.worknice.com/reconfig",
       },
-      { status: 200 },
+      { status: 200 }
     );
+  } catch (error) {
+    const message = error instanceof Error ? error.message : `${error}`;
+
+    return new NextResponse(message, {
+      status: 500,
+    });
+  }
+};
+
+
+
