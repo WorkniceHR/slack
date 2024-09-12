ebf88a027657371bf45df7d833b03f6e0f786b5d
matthewknee <matt@worknice.com>
2024-09-12 13:10:30 +1000

    removed whiteboard references

--- src/app/reconfig/page.tsx
+++ src/app/reconfig/page.tsx
@@ -1,36 +1,3 @@
-"use client";
-
-import Action from "@worknice/whiteboard/controls/Action";
-import Button from "@worknice/whiteboard/controls/Button";
-import ButtonLink from "@worknice/whiteboard/controls/ButtonLink";
-import Disclosure from "@worknice/whiteboard/controls/Disclosure";
-import Link from "@worknice/whiteboard/controls/Link";
-import RegularField from "@worknice/whiteboard/forms/RegularField";
-import StringInput from "@worknice/whiteboard/inputs/StringInput";
-import ActionSet from "@worknice/whiteboard/presentation/ActionSet";
-import Badge from "@worknice/whiteboard/presentation/Badge";
-import Banner from "@worknice/whiteboard/presentation/Banner";
-import Card from "@worknice/whiteboard/presentation/Card";
-import CardContent from "@worknice/whiteboard/presentation/CardContent";
-import ConfirmationModal from "@worknice/whiteboard/presentation/ConfirmationModal";
-import DefinitionList from "@worknice/whiteboard/presentation/DefinitionList";
-import DefinitionListItem from "@worknice/whiteboard/presentation/DefinitionListItem";
-import Divider from "@worknice/whiteboard/presentation/Divider";
-import Header from "@worknice/whiteboard/presentation/Header";
-import Icon from "@worknice/whiteboard/presentation/Icon";
-import PlainText from "@worknice/whiteboard/presentation/PlainText";
-import RichText from "@worknice/whiteboard/presentation/RichText";
-import Sheet from "@worknice/whiteboard/presentation/Sheet";
-import SheetContent from "@worknice/whiteboard/presentation/SheetContent";
-import SheetHeader from "@worknice/whiteboard/presentation/SheetHeader";
-import Subheading from "@worknice/whiteboard/presentation/Subheading";
-import TimelineItem from "@worknice/whiteboard/presentation/TimelineItem";
-import Title from "@worknice/whiteboard/presentation/Title";
-import VStack from "@worknice/whiteboard/presentation/VStack";
-import { Fragment } from "react";
-import SelectDemo from "./SelectDemo";
-
-
 return (
   <div>
     <p>Nothing to configure.</p>
