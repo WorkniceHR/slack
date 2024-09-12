d11808faca9e280b4f6f2125aa6d73300563922b
matthewknee <matt@worknice.com>
2024-09-12 13:06:00 +1000

    removed whiteboard

--- src/app/reconfig/layout.tsx
+++ src/app/reconfig/layout.tsx
@@ -1,15 +1,11 @@
-import "@worknice/whiteboard/shared.css";
-
-import brandFont from "@/fonts/brandFont";
-import BaseLayout from "@worknice/whiteboard/layouts/BaseLayout";
-import type { ReactNode } from "react";
-
-type Props = {
-  children: ReactNode;
-};
-
-const MinimalRootLayout = ({ children }: Props) => (
-  <BaseLayout brandTypeface={brandFont.style.fontFamily}>{children}</BaseLayout>
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
 );
 
-export default MinimalRootLayout;
+export default RootLayout;
