2071cafac977eeee6d61c39fabd01a49e07a8818
matthewknee <matt@worknice.com>
2024-09-12 17:22:20 +1000

    fixed syntax error

--- src/app/worknice/get-reconfiguration-url/route.ts
+++ src/app/worknice/get-reconfiguration-url/route.ts
@@ -5,19 +5,12 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 export const POST = async (request: NextRequest): Promise<NextResponse> => {
-  //const { log } = request;
   try {
-    // log.info("Parsing request…");
-
     const data = requestSchema.parse(await request.json());
-
-    //log.info("Generating authorization code…");
 
     const authorizationCode = crypto.randomBytes(16).toString("hex");
 
     await redis.setIntegrationId(authorizationCode, data.integrationId);
-
-    //log.info("Complete.");
 
     return NextResponse.json(
       {
@@ -25,18 +18,16 @@ export const POST = async (request: NextRequest): Promise<NextResponse> => {
           config.sessionCodeParam
         }=${authorizationCode}`,
       },
-      { status: 200 },
+      { status: 200 }
     );
   } catch (error) {
     const message = error instanceof Error ? error.message : `${error}`;
-
-    //log.error(message);
 
     return new NextResponse(message, {
       status: 400,
     });
   }
-});
+};
 
 const requestSchema = z.object({
   integrationId: z.string(),
