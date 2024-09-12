ed4720d9a07ea5aacecef15cf0a68f681fddd5a9
matthewknee <matt@worknice.com>
2024-09-12 17:46:23 +1000

    reverted changes

--- src/redis.ts
+++ src/redis.ts
@@ -1,16 +1,9 @@
 import { Redis } from "@upstash/redis";
 import config from "./config";
 
-const client = new Redis({
+const redis = new Redis({
   url: config.redis.restApiUrl,
   token: config.redis.restApiToken,
 });
-
-const redis = {
-  setIntegrationId: (sessionCode: string, value: string) =>
-    client.set(`session_code_integration_id:${sessionCode}`, value, {
-      ex: config.sessionCodeExpiry,
-    }),
-};
 
 export default redis;
