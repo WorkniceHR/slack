0c968bb47eddbbfee976a730983cc214fc7bbdec
matthewknee <matt@worknice.com>
2024-09-12 17:33:44 +1000

    Added getIntegrationId

--- src/redis.ts
+++ src/redis.ts
@@ -1,9 +1,14 @@
 import { Redis } from "@upstash/redis";
 import config from "./config";
 
-const redis = new Redis({
+const client = new Redis({
   url: config.redis.restApiUrl,
   token: config.redis.restApiToken,
 });
+
+const redis = {
+  getIntegrationId: (sessionCode: string) =>
+    client.get<string>(`session_code_integration_id:${sessionCode}`),
+};
 
 export default redis;
