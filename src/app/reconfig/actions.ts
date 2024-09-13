5689f3a8bade6671e086f56b7c05e5fae82b163d
matthewknee <matt@worknice.com>
2024-09-13 18:18:47 +1000

    Added birthday alerts

--- src/app/reconfig/actions.ts
+++ src/app/reconfig/actions.ts
@@ -1,4 +1,4 @@
-"use server"; // Ensure this is only runs server-side
+"use server"; // Ensure this only runs server-side
 
 import { revalidatePath } from "next/cache";
 import redis from "../../redis";
@@ -6,10 +6,16 @@ import redis from "../../redis";
 export const saveSelectedChannel = async (formData: FormData) => {
   const integrationId = formData.get("integrationId");
   const personActivatedChannel = formData.get("personActivatedChannel");
+  const personBirthdayChannel = formData.get("personBirthdayChannel"); // New addition
 
   await redis.set(
     `slack_channel:person_activated:${integrationId}`,
     personActivatedChannel
+  );
+
+  await redis.set( // New addition to handle the birthday channel
+    `slack_channel:person_birthday:${integrationId}`,
+    personBirthdayChannel
   );
 
   revalidatePath("/reconfig");
