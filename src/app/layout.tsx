5a0887bd747f77c27a6c599145efa2b0a7e5e6d2
Haydn Ewers <27211+haydn@users.noreply.github.com>
2024-09-19 14:03:17 +1000

    add basic styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,39 @@
+const css = (s: TemplateStringsArray) => s.join("");
+
 const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => (
   <html lang="en">
+    <head>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/@worknice/whiteboard@0.0.2/dist/shared.css"
+      />
+      <style>
+        {css`
+          .Container {
+            display: grid;
+            justify-content: center;
+            grid-template-columns: minmax(auto, 600px);
+          }
+          .Card {
+            background: white;
+            padding: 1em;
+            border-radius: 10px;
+          }
+          h1,
+          h2,
+          label,
+          button,
+          input,
+          select {
+            all: revert;
+          }
+        `}
+      </style>
+    </head>
     <body>{children}</body>
   </html>
 );
