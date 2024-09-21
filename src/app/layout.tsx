8baa03186d2ce69e585570ef5c50823b216d98ae
matthewknee <matt@worknice.com>
2024-09-21 05:51:30 +0000

    fixed a few layout issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ const RootLayout = ({
         {css`
         body {
           color: oklch(0.3886, 0.0206, 259.7);
+          font-family: -apple-system, "system-ui", "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 20px;
            }
           .Container {
             display: grid;
@@ -41,7 +46,7 @@ const RootLayout = ({
             border-radius: 8px;
             padding: 10px 20px 20px 20px;
             margin: 20px;
-            width: 80%;
+            width: 100%;
             max-width: 600px;
             margin: 0 auto; 
             border: 1px solid #E0E0E0;
@@ -61,7 +66,6 @@ const RootLayout = ({
             padding: 8px 32px 8px 12px;
             border: 1px solid #ccc;
             border-radius: 4px;
-            background: #fff url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 24 24' fill='none' stroke='%23000' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpolyline points='6 9 12 15 18 9'%3E%3C/polyline%3E%3C/svg%3E") no-repeat right 10px center; /* Custom dropdown arrow using SVG */
         }
     
         .wn-button--primary {
