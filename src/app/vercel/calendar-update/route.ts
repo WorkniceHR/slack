b05cfbd0a04471a50ddbc1d4300762400ac9a9a9
matthewknee <matt@worknice.com>
2024-09-28 03:48:11 +0000

    check if integration is archived

--- src/app/vercel/calendar-update/route.ts
+++ src/app/vercel/calendar-update/route.ts
@@ -5,6 +5,82 @@ import { createZodFetcher } from "zod-fetch";
 import redis from "../../../redis";
 import config from "@/config";
 
+
+
+// Send daily summary of events to Slack channels
+export const GET = async (request: NextRequest): Promise<NextResponse> => {
+  try {
+    const integrationIds = await getWorkniceIntegrationIds();
+
+    const channels: Array<string> = [];
+    const slackTokens: Array<string> = [];
+    const workniceTokens: Array<string> = [];
+    const calendarEvents: Array<CalendarEvent[]> = [];
+
+    for (const integrationId of integrationIds) {
+      const channel = await redis.get(`slack_channel:calendar_update:${integrationId}`);
+      if (typeof channel !== "string") {
+        console.log(`No Slack channel found for integration ${integrationId}. Skipping.`);
+        continue;
+      }
+
+      const slackToken = await redis.get(`slack_access_token:${integrationId}`);
+      if (typeof slackToken !== "string") {
+        console.log(`No Slack token found for integration ${integrationId}. Skipping.`);
+        continue;
+      }
+
+      const workniceToken = await redis.get(`worknice_api_key:${integrationId}`);
+      if (typeof workniceToken !== "string") {
+        console.log(`No Worknice token found for integration ${integrationId}. Skipping.`);
+        continue;
+      }
+
+
+      // Fetch the list of integrations and check if the current integration is archived
+      const integrations = await getWorkniceIntegrations(workniceToken);
+      const integration = integrations.find((i) => i.id === integrationId);
+
+      if (!integration || integration.archived) {
+        console.log(`Integration ${integrationId} is archived. Skipping.`);
+        continue;
+      }
+
+      channels.push(channel);
+      slackTokens.push(slackToken);
+      workniceTokens.push(workniceToken);
+
+      const events = await getWorkniceCalendarEvents(workniceToken);
+      const todayEvents = filterTodayEvents(events);
+      calendarEvents.push(todayEvents);
+      console.log(`Fetched calendar events for integration ${integrationId}`);
+      console.log(`All Worknice Events ${JSON.stringify(events)}`);
+      console.log(`Filtered Worknice Events ${JSON.stringify(todayEvents)}`);
+
+      const message = formatEventMessage(todayEvents);
+      await sendSlackMessage(slackToken, channel, message);
+      console.log(`Sent Slack message for integration ${integrationId}`);
+    }
+
+    return NextResponse.json({
+      test: "hello world",
+      integrationIds: integrationIds,
+      channels: channels,
+      slackTokens: slackTokens,
+      workniceTokens: workniceTokens,
+      calendarEvents: calendarEvents,
+    });
+  } catch (error) {
+    const message = error instanceof Error ? error.message : `${error}`;
+
+    return new NextResponse(message, {
+      status: 500,
+    });
+  }
+};
+
+const fetchWithZod = createZodFetcher();
+
 async function getWorkniceIntegrationIds(): Promise<string[]> {
   const keys = await redis.keys("worknice_api_key:*");
   return keys
@@ -12,7 +88,49 @@ async function getWorkniceIntegrationIds(): Promise<string[]> {
     .filter((id): id is string => id !== undefined);
 }
 
-const fetchWithZod = createZodFetcher();
+// Get the Worknice integrations so we can check whether they are archived or not
+async function getWorkniceIntegrations(apiKey: string): Promise<{ id: string, archived: boolean }[]> {
+  const response = await fetchWithZod(
+    z.object({
+      data: z.object({
+        session: z.object({
+          org: z.object({
+            integrations: z.array(
+              z.object({
+                id: z.string(),
+                archived: z.boolean(),
+              })
+            ),
+          }),
+        }),
+      }),
+    }),
+    `${config.worknice.baseUrl}/api/graphql`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "worknice-api-token": apiKey,
+      },
+      body: JSON.stringify({
+        query: `
+          query Integrations {
+            session {
+              org {
+                integrations(includeArchived: true) {
+                  id
+                  archived
+                }
+              }
+            }
+          }
+        `,
+      }),
+    }
+  );
+  return response.data.session.org.integrations;
+}
+
 
 const workniceCalendarEventsSchema = z.object({
   data: z.object({
@@ -38,6 +156,9 @@ const workniceCalendarEventsSchema = z.object({
   }),
 });
 
+
+
+// Get the calendar events from Worknice
 async function getWorkniceCalendarEvents(apiKey: string): Promise<any[]> {
   const response = await fetchWithZod(
     workniceCalendarEventsSchema,
@@ -73,6 +194,7 @@ async function getWorkniceCalendarEvents(apiKey: string): Promise<any[]> {
   
 }
 
+// Send a Slack message to the specified Slack channel
 async function sendSlackMessage(
   token: string,
   channel: string,
@@ -123,7 +245,7 @@ function filterTodayEvents(events: CalendarEvent[]): CalendarEvent[] {
 }
 
 
-
+// Format the calendar events into a Slack message
 function formatEventMessage(events: CalendarEvent[]): string {
   if (events.length === 0) {
     return "No calendar updates for today :sunny:";
@@ -174,63 +296,3 @@ function formatEventMessageSet(
   return "";
 }
 
-export const GET = async (request: NextRequest): Promise<NextResponse> => {
-  try {
-    const integrationIds = await getWorkniceIntegrationIds();
-
-    const channels: Array<string> = [];
-    const slackTokens: Array<string> = [];
-    const workniceTokens: Array<string> = [];
-    const calendarEvents: Array<CalendarEvent[]> = [];
-
-    for (const integrationId of integrationIds) {
-      const channel = await redis.get(`slack_channel:calendar_update:${integrationId}`);
-      if (typeof channel !== "string") {
-        console.log(`No Slack channel found for integration ${integrationId}. Skipping.`);
-        continue;
-      }
-
-      const slackToken = await redis.get(`slack_access_token:${integrationId}`);
-      if (typeof slackToken !== "string") {
-        console.log(`No Slack token found for integration ${integrationId}. Skipping.`);
-        continue;
-      }
-
-      const workniceToken = await redis.get(`worknice_api_key:${integrationId}`);
-      if (typeof workniceToken !== "string") {
-        console.log(`No Worknice token found for integration ${integrationId}. Skipping.`);
-        continue;
-      }
-
-      channels.push(channel);
-      slackTokens.push(slackToken);
-      workniceTokens.push(workniceToken);
-
-      const events = await getWorkniceCalendarEvents(workniceToken);
-      const todayEvents = filterTodayEvents(events);
-      calendarEvents.push(todayEvents);
-      console.log(`Fetched calendar events for integration ${integrationId}`);
-      console.log(`All Worknice Events ${JSON.stringify(events)}`);
-      console.log(`Filtered Worknice Events ${JSON.stringify(todayEvents)}`);
-
-      const message = formatEventMessage(todayEvents);
-      await sendSlackMessage(slackToken, channel, message);
-      console.log(`Sent Slack message for integration ${integrationId}`);
-    }
-
-    return NextResponse.json({
-      test: "hello world",
-      integrationIds: integrationIds,
-      channels: channels,
-      slackTokens: slackTokens,
-      workniceTokens: workniceTokens,
-      calendarEvents: calendarEvents,
-    });
-  } catch (error) {
-    const message = error instanceof Error ? error.message : `${error}`;
-
-    return new NextResponse(message, {
-      status: 500,
-    });
-  }
-};
