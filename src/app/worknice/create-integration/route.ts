349cc1b098ac74916558fe545fa7260e9a3cf690
Haydn Ewers <27211+haydn@users.noreply.github.com>
2024-10-09 11:35:42 +1100

    update createIntegration route to use the js-sdk

--- src/app/worknice/create-integration/route.ts
+++ src/app/worknice/create-integration/route.ts
@@ -1,66 +1,20 @@
 import config from "@/config";
-import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
-import { createZodFetcher } from "zod-fetch";
-import redis from "../../../redis";
+import redis from "@/redis";
+import { handleCreateIntegrationWebhook } from "@worknice/js-sdk/helpers";
 
-const fetchWithZod = createZodFetcher();
-
-export const POST = async (request: NextRequest): Promise<NextResponse> => {
-  try {
-    console.log("Parsing request…");
-
-    const data = z
-      .object({
-        apiToken: z.string(),
-        integrationId: z.string(),
-      })
-      .parse(await request.json());
-
-    console.log("Saving token…");
-
-    await redis.set(`worknice_api_key:${data.integrationId}`, data.apiToken);
-
-    console.log("Initializing integration…");
-
-    await fetchWithZod(
-      z.object({
-        data: z.object({
-          initializeIntegration: z.object({
-            id: z.string(),
-          }),
-        }),
-      }),
-      `${config.worknice.baseUrl}/api/graphql`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "worknice-api-token": data.apiToken,
-        },
-        body: JSON.stringify({
-          query: `
-            mutation InitializeIntegration($id: ID!) {
-              initializeIntegration(integrationId: $id) {
-                id
-              }
-            }
-          `,
-          variables: { id: data.integrationId },
-        }),
-      }
-    );
-
-    console.log("Done.");
-
-    return new NextResponse("ok", {
-      status: 200,
-    });
-  } catch (error) {
-    const message = error instanceof Error ? error.message : `${error}`;
-
-    return new NextResponse(message, {
-      status: 500,
-    });
-  }
-};
+export const POST = async (request: Request): Promise<Response> =>
+  handleCreateIntegrationWebhook(
+    request,
+    {
+      persistDetails: async ({ payload }) => {
+        await redis.set(
+          `worknice_api_key:${payload.integrationId}`,
+          payload.apiToken
+        );
+      },
+    },
+    {
+      apiUrl: `${config.worknice.baseUrl}/api/graphql`,
+      debug: true,
+    }
+  );
