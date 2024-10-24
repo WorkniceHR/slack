54d93b4d2e14a9a1750f30bd864d98409b0ef2a1
Haydn Ewers <27211+haydn@users.noreply.github.com>
2024-10-24 15:26:54 +1100

    use after() to delay delegation of slash command

--- src/app/slash-commands/route.ts
+++ src/app/slash-commands/route.ts
@@ -1,4 +1,6 @@
+import config from "@/config";
 import { handleRequestWithWorknice } from "@worknice/js-sdk/helpers";
+import { unstable_after as after } from "next/server";
 import { parse } from "querystring";
 import { z } from "zod";
 
@@ -9,25 +11,30 @@ export const POST = async (request: Request) =>
   >(request, {
     getApiToken: async () => "",
     getEnv: async () => null,
-    handleRequest: async ({ logger, payload }) => {
-      fetch(`https://slack.worknice.com/slash-commands/${payload.command}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload),
-      }).then((res) => {
-        if (!res.ok) {
-          logger.error("Failed to delegate slash command.");
-        }
+    handleRequest: async ({ payload }) => {
+      after(async () => {
+        await fetch(
+          `${config.baseUrl}/slash-commands/${HANDLERS[payload.command]}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload),
+          }
+        );
       });
       return { text: "Working..." };
     },
     parsePayload: async ({ request }) => {
       const text = await request.text();
-      const body = parse(text);
-      const data = slackRequestSchema.parse(body);
-      return data;
+      return slackRequestSchema.parse(parse(text));
     },
   });
+
+const HANDLERS: Record<z.infer<typeof slackRequestSchema>["command"], string> =
+  {
+    "/whois": "whois",
+    "/whosaway": "whosaway",
+  } as const;
 
 const slackRequestSchema = z.object({
   command: z.union([z.literal("/whois"), z.literal("/whosaway")]),
